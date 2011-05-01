822a278de33982b4fff987973600987d73ede6ab
Bediako George <bediako.george@lucidtechnics.com>
2011-05-01 12:35:08 -0400

    removed unecessary files

--- src/java/airlift/servlet/rest/GoogleUserServiceFactory.java
+++
@@ -1,7 +0,0 @@
-package airlift.servlet.rest;
-
-public class GoogleUserServiceFactory
-   implements UserServiceFactory
-{
-	public UserService getUserService(javax.servlet.http.HttpServletRequest _httpServletRequest) { return new GoogleUserService(); }
-}
--- src/java/airlift/servlet/rest/UserServiceFactory.java
+++
@@ -1,6 +0,0 @@
-package airlift.servlet.rest;
-
-public interface UserServiceFactory
-{
-	public UserService getUserService(javax.servlet.http.HttpServletRequest _httpServletRequest);
-}
