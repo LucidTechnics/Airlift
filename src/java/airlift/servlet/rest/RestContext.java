1bee314c90d9dd86fc39f14bd1e057e65547e598
Bediako George <bediako.george@lucidtechnics.com>
2011-07-09 15:17:52 -0400

    Reversed javadoc changes in RestContext.

--- src/java/airlift/servlet/rest/RestContext.java
+++ src/java/airlift/servlet/rest/RestContext.java
@@ -334,20 +334,11 @@ public class RestContext
 		return Boolean.valueOf((getThisDomainIds().isEmpty() == false));
 	}
 
-<<<<<<< local
 	public String getParameter(String _id)
 	{
 		return getIdValue(_id);
 	}
 
-=======
-	/**
-	 * Gets the id value.
-	 *
-	 * @param _id the _id
-	 * @return the id value
-	 */
->>>>>>> other
 	public String getIdValue(String _id)
 	{
 		return (String)getUriParameterMap().get(_id.toLowerCase());
